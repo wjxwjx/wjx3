31351c1c88650477d8d5464f2c7db3d578c79e37
wangjinxiang <840375146@qq.com>
2015-12-17 23:25:03 +0800

    提交项目

---
+++ 教室管理系统项目/1/classroom/urls.py
@@ -0,0 +1,24 @@
+﻿from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from room.views import *
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'classroom.views.home', name='home'),
+    # url(r'^classroom/', include('classroom.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    (r'^$',Login),
+    (r'^sign/$',Sign),
+    (r'^showdetails1/$',show1),
+    (r'^showdetails2/$',show2),
+    (r'^showdetails3/$',show3),
+    (r'^showdetails4/$',show4),
+    url(r'^admin/', include(admin.site.urls)),
+)
